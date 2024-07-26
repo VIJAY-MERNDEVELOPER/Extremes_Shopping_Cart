f7083b66e714390947511ff0bc957c580d67b76a
VIJAY-MERNDEVELOPER <142765666+VIJAY-MERNDEVELOPER@users.noreply.github.com>
2024-07-26 11:18:24 +0530

    product images updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -20,47 +20,7 @@ import SideBar from "./SideBar";
 import axios from "axios";
 import { Link, NavLink } from "react-router-dom";
 
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  color: "black",
-  borderRadius: 50,
-  backgroundColor: "whitesmoke",
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(3),
-    width: "auto",
-  },
-}));
 
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  color: "black",
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
-  },
-}));
 
 function NavBar({ setIsLoggedIn, isLoggedIn }) {
   const [anchorEl, setAnchorEl] = useState(null);
